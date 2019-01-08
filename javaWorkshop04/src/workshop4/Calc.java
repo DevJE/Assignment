acdcffc61c636d81439b150a1a13a269014cc5ef
DevJE <45855412+DevJE@users.noreply.github.com>
2019-01-09 05:10:34 +0900

    Update Calc.java

--- javaWorkshop04/src/workshop4/Calc.java
+++ javaWorkshop04/src/workshop4/Calc.java
@@ -4,7 +4,7 @@ public class Calc {
 	public int calculate(int data) {
 		int sum = 0;
 		int n = 1;
-		System.out.print("Â¦¼ö : ");
+		System.out.print("ì§ìˆ˜ : ");
 		while(n <= data) {
 			if(n%2 == 0)
 				System.out.print(n + " ");
